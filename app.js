151f768c5948b71283017176df5812d17c98faa9
srav13 <87898928+srav13@users.noreply.github.com>
2021-08-07 20:39:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var buttoninnerhtml=this.innerHTML;
+
+        makesound(buttoninnerhtml);
+        buttonanimation(buttoninnerhtml);
+    });
+    
+}
+document.addEventListener("keydown",function(event){
+   makesound(event.key);
+   buttonanimation(event.key);
+});
+
+function makesound(key)
+{
+    switch (key)
+    {
+        case "w":
+            var tom1=new Audio("tom-1.mp3");
+            tom1.play();
+        break;
+        case "a":
+            var tom2=new Audio("tom-2.mp3");
+            tom2.play();
+        break;
+        case "s":
+            var tom3=new Audio("tom-3.mp3");
+            tom3.play();
+        break;
+        case "d":
+            var tom4=new Audio("tom-4.mp3");
+            tom4.play();
+        break;
+        case "j":
+            var tom4=new Audio("snare.mp3");
+            tom4.play();
+        break;
+        case "k":
+            var tom4=new Audio("crash.mp3");
+            tom4.play();
+        break;
+        case "l":
+            var tom4=new Audio("kick-bass.mp3");
+            tom4.play();
+        break;   
+    }
+}
+function buttonanimation(currentkey)
+{
+    var activebtn=document.querySelector("."+currentkey);
+    activebtn.classList.add("pressed");
+    setTimeout(function(){
+        activebtn.classList.remove("pressed");
+    },100);
+}
